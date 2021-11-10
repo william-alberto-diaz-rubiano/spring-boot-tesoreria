d2a8bd10285151d4ad2d1e0a23c33bf2d0d0b2f8
Francis Esquivel <fesquivelc@gmail.com>
2021-11-09 19:50:18 -0500

    [FIX] Corrigiendo vulnerabilidades encontradas por SonarQube

--- src/main/java/pe/gob/vuce/zee/api/maestros/base/Constantes.java
+++ src/main/java/pe/gob/vuce/zee/api/maestros/base/Constantes.java
@@ -1,26 +1,23 @@
 package pe.gob.vuce.zee.api.maestros.base;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class Constantes {
-    public static int TIPO_ETAPA = 1;
-    public static int TIPO_MANZANA = 2;
-    public static int TIPO_BLOQUE = 3;
+    private Constantes() {
+    }
 
-    public static int HABILITADO = 0;
-    public static int DESHABILITADO = 9;
+    public final static int HABILITADO = 0;
+    public final static int DESHABILITADO = 9;
 
-    public static String CONTENT_TYPE_XLSX = "application/vnd.ms-excel";
-    public static String CONTENT_TYPE_CSV = "text/csv";
+    public final static String CONTENT_TYPE_XLSX = "application/vnd.ms-excel";
+    public final static String CONTENT_TYPE_CSV = "text/csv";
 
-    public static UUID UID_TEST = UUID.fromString("00000000-0000-0000-0000-000000000000");
+    public final static UUID UID_TEST = UUID.fromString("00000000-0000-0000-0000-000000000000");
 
-    public static int NO_ERROR = 0;
+    public final static int NO_ERROR = 0;
 
-    public static String RENIEC_APLICACION = "ZEE2";
+    public final static String RENIEC_APLICACION = "ZEE2";
 
     public static final Pattern DNI_REGEX = Pattern.compile("^[0-9]{8}$");
     public static final Pattern RUC_REGEX = Pattern.compile("^[0-9]{11}$");
